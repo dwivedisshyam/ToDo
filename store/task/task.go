fe179efe58b23a451cbff560ed5d1ea7ab7c885d
Shyam <dwivedisshyam@gmail.com>
2023-03-15 00:58:51 +0530

    fix task queries

--- store/task/task.go
+++ store/task/task.go
@@ -9,21 +9,21 @@ import (
 )
 
 const (
-	createQuery = `INSERT INTO "user" (user_id, title, description, due_date, created_at) VALUES ($1, $2, $3, $4, $5)`
-	getQuery    = `SELECT user_id, title, description, due_date, created_at from "user" WHERE id=$1`
-	getAllQuery = `SELECT id, user_id, title, description, due_date, created_at from "user"`
-	updateQuery = `UPDATE "user" SET title=$1, description=$2, due_date=$3 WHERE id=$4`
+	createQuery = `INSERT INTO "task" (user_id, title, description, due_date, created_at) VALUES ($1, $2, $3, $4, $5)`
+	getQuery    = `SELECT user_id, title, description, due_date, created_at from "task" WHERE id=$1`
+	getAllQuery = `SELECT id, user_id, title, description, due_date, created_at from "task"`
+	updateQuery = `UPDATE "task" SET title=$1, description=$2, due_date=$3 WHERE id=$4`
 )
 
-type userStore struct {
+type taskStore struct {
 	db *sql.DB
 }
 
 func New(db *sql.DB) store.Task {
-	return &userStore{db: db}
+	return &taskStore{db: db}
 }
 
-func (us *userStore) Create(t *model.Task) error {
+func (us *taskStore) Create(t *model.Task) error {
 	_, err := us.db.Exec(createQuery, t.UserID, t.Title, t.Description, t.DueDate, t.CreatedAt)
 	if err != nil {
 		return errors.Unexpected(err.Error())
@@ -32,7 +32,7 @@ func (us *userStore) Create(t *model.Task) error {
 	return nil
 }
 
-func (us *userStore) Update(t *model.Task) error {
+func (us *taskStore) Update(t *model.Task) error {
 	_, err := us.db.Exec(updateQuery, t.Title, t.Description, t.DueDate, t.ID)
 	if err != nil {
 		return errors.Unexpected(err.Error())
@@ -41,7 +41,7 @@ func (us *userStore) Update(t *model.Task) error {
 	return nil
 }
 
-func (us *userStore) List() (model.Tasks, error) {
+func (us *taskStore) List() (model.Tasks, error) {
 	rows, err := us.db.Query(getAllQuery)
 	if err != nil {
 		return nil, errors.Unexpected(err.Error())
@@ -63,7 +63,7 @@ func (us *userStore) List() (model.Tasks, error) {
 	return tasks, nil
 }
 
-func (us *userStore) Get(id int64) (*model.Task, error) {
+func (us *taskStore) Get(id int64) (*model.Task, error) {
 	var t model.Task
 
 	err := us.db.QueryRow(getQuery, id).Scan(&t.UserID, &t.Title, &t.Description, &t.DueDate, &t.CreatedAt)
